fc8592b53d9d94f548620ac7f252c5167574457d
izaacyo <mariusppetrut@gmail.com>
2021-09-27 13:08:33 +0300

    displayed shoes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import './App.css';
+import data from "../src/shoes.json";
+import Products from './components/Products';
 
-import Main from "./components/Main/Main"
+
+/*import Main from "./components/Main/Main"
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import { useEffect } from 'react';
 import { intializeShoes } from './store/actions/actions';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';*/
 
-const App = () => {
+/*const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -27,6 +31,40 @@ const App = () => {
       <Footer />
     </div>
   );
+}*/
+
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/"> React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar"> Cart Items</div>
+
+          </div>
+        </main>
+        <footer>
+          All rights reserved
+        </footer>
+
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
