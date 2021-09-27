bb05e624d83a0033dc6169cada97ac9999a209b5
izaacyo <mariusppetrut@gmail.com>
2021-09-27 13:56:31 +0300

    added currency

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import formatCurrency from '../util';
 
 export default class Products extends Component {
     render() {
@@ -16,7 +17,7 @@ export default class Products extends Component {
                                 </a>
                                 <div className="product-price">
                                     <div>
-                                        {product.price}
+                                        {formatCurrency(product.price)}
                                     </div>
                                     <button className="button primary"> Add to Cart </button>
                                 </div>
