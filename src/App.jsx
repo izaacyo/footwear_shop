a78cbe8edcff19a55084e00a5a67380e1672144a
izaacyo <mariusppetrut@gmail.com>
2021-10-07 13:06:55 +0300

    Register page  styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 
 const App = () => {
-  return <Product/>
+  return <Register/>
 }
     
 
